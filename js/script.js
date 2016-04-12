f68df66f4c68aec08dcfa45b1d65756148abc6d1
polmanovic <polman@MacBook-Pro-van-polman.local>
2016-04-12 14:33:17 +0200

    bezig met responsive menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 
-$(document).ready(function(){
-    $(".loadMore-btn").click(function(){
-        $(".mustDoWhite").toggle(1000);
+jQuery(document).ready(function() {
+
+	//active class toevoegen en verwijderen
+    jQuery('.toggle-nav').click(function(e) {
+
+    	//toggle nav wel of niet weergeven
+        jQuery(this).toggleClass('active');
+
+        //menu ul wel of niet weergeven
+		jQuery('.menu ul').toggleClass('active');
+ 
+ 		// verwijdert het standaard gedrag van toggle nav
+ 		// omdat het een anker is zou de pagina normaal naar de top verspringen
+        e.preventDefault();
     });
 });
+
